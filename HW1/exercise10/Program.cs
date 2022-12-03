4430fce2f6cd326629d486fe63cb2529adf74902
ExcitaitoOne <roja28@yandex.ru>
2022-12-03 13:08:53 +0700

    OverWork

--- HW1/exercise10/Program.cs
+++
@@ -1,9 +0,0 @@
-﻿// Удалить вторую цифру трёхзначного числа
-Console.WriteLine("Введите трехзначное число");
-int A = int.Parse(Console.ReadLine() ?? "0");
-int one;
-int three;
-one = A /100;
-three = A%10;
-
-Console.WriteLine("Число без второй цифры " + one+ three);
